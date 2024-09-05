b5766aec749639d26a2fe185069608ba63223e92
4zeddine <azeddine.nader@gmail.com>
2024-09-05 12:27:12 +0100

    Session auth commit

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -8,32 +8,30 @@ from api.v1.views import app_views
 from flask import abort, jsonify, request
 
 
-@app_views.route('/auth_session/login/',
-                 methods=['POST'],
-                 strict_slashes=False)
+@app_views.route('/auth_session/login', methods=['POST'], strict_slashes=False)
 def login() -> Tuple[str, int]:
     """Login route
     Returns:
         User object in json
     """
     email = request.form.get('email')
-    passwrd = request.form.get('password')
-    if email is None or email == '':
+    if email is None or len(email.strip()) == 0:
         return jsonify({"error": "email missing"}), 400
-    if passwrd is None or passwrd == '':
+    passwrd = request.form.get('password')
+    if passwrd is None or len(passwrd.strip()) == 0:
         return jsonify({"error": "password missing"}), 400
     not_found = {"error": "no user found for this email"}
+    pass_error = {"error": "wrong password"}
     try:
         get_users = User.search({'email': email})
     except Exception:
         return jsonify(not_found), 404
-    if get_users <= 0:
+    if len(get_users) <= 0:
         return jsonify(not_found), 404
-    pass_error = {"error": "wrong password"}
     if get_users[0].is_valid_password(passwrd):
         from api.v1.app import auth
         new_session = auth.create_session(getattr(get_users[0], 'id'))
-        dict = jsonify(get_users[0].to_json())
-        dict.set_cookie(os.getenv("SESSION_NAME"), new_session)
-        return dict
+        res_dict = jsonify(get_users[0].to_json())
+        res_dict.set_cookie(os.getenv("SESSION_NAME"), new_session)
+        return res_dict
     return jsonify(pass_error), 401
