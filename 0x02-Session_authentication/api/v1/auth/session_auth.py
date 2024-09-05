314612e319c24f8129b11e94e442e16738745605
4zeddine <azeddine.nader@gmail.com>
2024-09-05 13:12:29 +0100

    Session auth commit

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -5,7 +5,7 @@ Session Authentication module for the API
 from .auth import Auth
 from models.user import User
 from uuid import uuid4
-from flask import request
+from flask import request, session
 
 
 class SessionAuth(Auth):
@@ -35,3 +35,17 @@ class SessionAuth(Auth):
         req_cookie = self.session_cookie(request)
         user_id = self.user_id_for_session_id(req_cookie)
         return User.get(user_id)
+
+    def destroy_session(self, request=None):
+        """Destroys a session"""
+        if request is None:
+            return False
+        session_req = self.session_cookie(request)
+        if session_req is None:
+            return False
+        user_id = self.user_id_for_session_id(session_req)
+        if user_id is None:
+            return False
+        if session_req in self.user_id_by_session_id:
+            del self.user_id_by_session_id[session_req]
+        return True
--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -9,7 +9,7 @@ from flask import abort, jsonify, request
 
 
 @app_views.route('/auth_session/login', methods=['POST'], strict_slashes=False)
-def login() -> Tuple[str, int]:
+def user_login() -> Tuple[str, int]:
     """Login route
     Returns:
         User object in json
@@ -35,3 +35,15 @@ def login() -> Tuple[str, int]:
         res_dict.set_cookie(os.getenv("SESSION_NAME"), new_session)
         return res_dict
     return jsonify(pass_error), 401
+
+@app_views.route('/auth_session/logout', methods=['DELETE'], strict_slashes=False)
+def user_logout() -> Tuple[str, int]:
+    """Route for Logout
+    Returns:
+        empty Json object
+    """
+    from api.v1.app import auth
+    deleted_session = auth.destroy_session(request)
+    if not deleted_session:
+        abort(404)
+    return jsonify({}), 200
